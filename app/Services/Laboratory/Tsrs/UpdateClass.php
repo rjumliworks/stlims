1dc20c1379b11abb61e26bbb87e1ce6ff4a51c7e
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-05-13 09:17:02 +0800

    asd

--- app/Services/Laboratory/Tsrs/UpdateClass.php
+++ app/Services/Laboratory/Tsrs/UpdateClass.php
@@ -24,7 +24,7 @@ class UpdateClass
 
     public function update($request){
         $data = Tsr::with('payment')->where('id',$request->id)->first();
-        if($data->status_id == 1 || $data->status_id == 2){
+        if($data){
             $data->customer_id = $request->customer['value'];
             $data->conforme_id = $request->conforme['value'];
             $data->purpose_id = $request->purpose_id;
