e588bba24852129aca447a92aef41156bdf8cc7f
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-05-14 14:13:08 +0800

    asd

--- app/Services/Executive/Users/SaveClass.php
+++ app/Services/Executive/Users/SaveClass.php
@@ -10,7 +10,7 @@ use App\Jobs\MailJob;
 class SaveClass
 {
     public function store($request){
-        $data = User::create(array_merge($request->all(), ['password' => bcrypt('123456789'), 'role' => 'Staff','avatar' =>'avatar.jpg']));
+        $data = User::create(array_merge($request->all(), ['password' => bcrypt('dost6!@#$%'), 'role' => 'Staff','avatar' =>'avatar.jpg']));
         $data->profile()->create($request->all());
         $data->myroles()->create($request->all());
         MailJob::dispatch($data);
