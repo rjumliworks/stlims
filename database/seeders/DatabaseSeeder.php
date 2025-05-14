af50ccc0b789503cb8df82f408a59720df40e1f9
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-05-14 14:09:51 +0800

    asd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,45 +12,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(ListDropdownsTableSeeder::class);
+        $this->call(ListDropdownsTableSeeder::class);
 
-        // \DB::table('users')->insert([
-        //     'username' => 'administrator',
-        //     'email' => 'kradjumli@gmail.com',
-        //     'password' => bcrypt('123456!@#$%'),
-        //     'role' => 'Administrator',
-        //     'is_active' => 1,
-        //     'email_verified_at' => '2024-05-15 08:46:33',
-        //     'created_at' => now(),
-        //     'updated_at' => now()
-        // ]);
+        \DB::table('users')->insert([
+            'username' => 'administrator',
+            'email' => 'kradjumli@gmail.com',
+            'password' => bcrypt('123456!@#$%'),
+            'role' => 'Administrator',
+            'is_active' => 1,
+            'email_verified_at' => '2024-05-15 08:46:33',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
 
-        // \DB::table('user_profiles')->insert([
-        //     'firstname' => 'Ra-ouf',
-        //     'lastname' => 'Jumli',
-        //     'middlename' => 'Indanan',
-        //     'avatar' => 'avatar.jpg',
-        //     'sex' => 'Male',
-        //     'user_id' => 1,
-        //     'mobile' => '09171531652',
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        // ]);
+        \DB::table('user_profiles')->insert([
+            'firstname' => 'Ra-ouf',
+            'lastname' => 'Jumli',
+            'middlename' => 'Indanan',
+            'avatar' => 'avatar.jpg',
+            'sex' => 'Male',
+            'user_id' => 1,
+            'mobile' => '09171531652',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
         
-        // $this->call(LocationRegionsTableSeeder::class);
-        // $this->call(LocationProvincesTableSeeder::class);
-        // $this->call(LocationMunicipalitiesTableSeeder::class);
-        // $this->call(LocationBarangaysTableSeeder::class);
+        $this->call(LocationRegionsTableSeeder::class);
+        $this->call(LocationProvincesTableSeeder::class);
+        $this->call(LocationMunicipalitiesTableSeeder::class);
+        $this->call(LocationBarangaysTableSeeder::class);
        
-        // $this->call(ListIndustriesTableSeeder::class);
-        // $this->call(ListLaboratoriesTableSeeder::class);
-        // $this->call(ListRolesTableSeeder::class);
-        // $this->call(ListStatusesTableSeeder::class);
-        // $this->call(ListDiscountsTableSeeder::class); 
-        // $this->call(MembersTableSeeder::class);
-        // $this->call(AgenciesTableSeeder::class);
-        // $this->call(AgencyAddressesTableSeeder::class);
-        // $this->call(AgencyConfigurationsTableSeeder::class);
+        $this->call(ListIndustriesTableSeeder::class);
+        $this->call(ListLaboratoriesTableSeeder::class);
+        $this->call(ListRolesTableSeeder::class);
+        $this->call(ListStatusesTableSeeder::class);
+        $this->call(ListDiscountsTableSeeder::class); 
+        $this->call(MembersTableSeeder::class);
+        $this->call(AgenciesTableSeeder::class);
+        $this->call(AgencyAddressesTableSeeder::class);
+        $this->call(AgencyConfigurationsTableSeeder::class);
     
         // $this->call(TestserviceNamesTableSeeder::class);
         // $this->call(TestserviceMethodsTableSeeder::class);
@@ -96,13 +96,13 @@ class DatabaseSeeder extends Seeder
         // $this->call(FinanceReceiptDetailsTableSeeder::class);
         // $this->call(FinanceDepositsTableSeeder::class);
 
-        $this->call(SchedulesTableSeeder::class);
-        $this->call(InventorySuppliersTableSeeder::class);
-        $this->call(InventoryItemsTableSeeder::class);
-        $this->call(InventoryStocksTableSeeder::class);
-        $this->call(InventoryWithdrawalsTableSeeder::class);
-        $this->call(EquipmentTableSeeder::class);
-        $this->call(EquipmentLogsTableSeeder::class);
-        $this->call(EquipmentInfosTableSeeder::class);
+        // $this->call(SchedulesTableSeeder::class);
+        // $this->call(InventorySuppliersTableSeeder::class);
+        // $this->call(InventoryItemsTableSeeder::class);
+        // $this->call(InventoryStocksTableSeeder::class);
+        // $this->call(InventoryWithdrawalsTableSeeder::class);
+        // $this->call(EquipmentTableSeeder::class);
+        // $this->call(EquipmentLogsTableSeeder::class);
+        // $this->call(EquipmentInfosTableSeeder::class);
     }
 }
