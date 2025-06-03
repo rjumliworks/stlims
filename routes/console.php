9913c997f0c8655a1a375ebd2f02cc7bdddf0f26
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-06-03 11:50:06 +0800

    aw

--- routes/console.php
+++ routes/console.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Artisan;
 
 Schedule::command('notify:due-tsrs')->weekdays()->dailyAt('08:00');
 Schedule::command('notify:schedule')->weekdays()->dailyAt('08:00');
-// Schedule::command('notify:due-tsrs')->weekdays()->dailyAt('12:00');
+Schedule::command('notify:due-tsrs')->weekdays()->dailyAt('12:00');
