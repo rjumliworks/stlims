05d85b4cd096d6ba736dd2b7250124c7b27e5b3a
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-07-10 12:09:45 +0800

    aw

--- resources/js/Pages/Modules/Insights/Top/Index.vue
+++ resources/js/Pages/Modules/Insights/Top/Index.vue
@@ -73,14 +73,14 @@
         <b-col lg>
             <div class="input-group mb-1">
                 <span class="input-group-text"> <i class="ri-search-line search-icon"></i></span>
-                <input type="text" placeholder="Search Request" class="form-control" style="width: 30%;">
-                <Multiselect class="white" style="width: 15%;" :options="types" v-model="laboratory" label="name" :allow-empty="false" :searchable="true" placeholder="Select Laboratory" />
-                <Multiselect v-if="by == 'By Semester'" class="white" style="width: 15%;" :options="semesters" v-model="semester" label="name" :allow-empty="false" :searchable="true" placeholder="Select Month" />
-                <Multiselect v-if="by == 'By Quarter'" class="white" style="width: 15%;" :options="quarters" v-model="quarter" label="name" :allow-empty="false" :searchable="true" placeholder="Select Month" />
-                <Multiselect v-if="by == 'By Month'" class="white" style="width: 15%;" :options="months" v-model="month" label="name" :allow-empty="false" :searchable="true" placeholder="Select Month" />
-                <Multiselect class="white" style="width: 15%;" :options="['By Month','By Quarter','By Semester']" v-model="by" label="name" :allow-empty="false" :searchable="true" placeholder="Filter By" />
-                <Multiselect class="white" style="width: 15%;" :options="['External','Internal']" v-model="customer" label="name" :allow-empty="false" :searchable="true" placeholder="Filter Customer" />
-                <Multiselect class="white" style="width: 15%;" :options="years" v-model="year" label="name" :searchable="true" placeholder="Select Year" />
+                <input type="text" placeholder="Search Request" class="form-control" style="width: 10%;">
+                <Multiselect class="white" style="width: 17%;" :options="types" v-model="laboratory" label="name" :allow-empty="false" :searchable="true" placeholder="Select Laboratory" />
+                <Multiselect v-if="by == 'By Semester'" class="white" style="width: 13%;" :options="semesters" v-model="semester" label="name" :allow-empty="false" :searchable="true" placeholder="Select Month" />
+                <Multiselect v-if="by == 'By Quarter'" class="white" style="width: 13%;" :options="quarters" v-model="quarter" label="name" :allow-empty="false" :searchable="true" placeholder="Select Month" />
+                <Multiselect v-if="by == 'By Month'" class="white" style="width: 13%;" :options="months" v-model="month" label="name" :allow-empty="false" :searchable="true" placeholder="Select Month" />
+                <Multiselect class="white" style="width: 12%;" :options="['By Month','By Quarter','By Semester']" v-model="by" label="name" :allow-empty="false" :searchable="true" placeholder="Filter By" />
+                <Multiselect class="white" style="width: 12%;" :options="['External','Internal']" v-model="customer" label="name" :allow-empty="false" :searchable="true" placeholder="Filter Customer" />
+                <Multiselect class="white" style="width: 12%;" :options="years" v-model="year" label="name" :searchable="true" placeholder="Select Year" />
                 <!-- <b-button @click="refresh()" type="button" variant="">
                     <i class="bx bx-refresh"></i> 
                 </b-button> -->
