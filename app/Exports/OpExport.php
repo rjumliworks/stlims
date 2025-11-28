a3f887bc086eaef87d45a330d32daedbf89d4c25
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-11-28 10:46:31 +0800

    Update OpExport.php

--- app/Exports/OpExport.php
+++ app/Exports/OpExport.php
@@ -24,11 +24,16 @@ class OpExport implements FromView
         ->with(['items' => function ($query) {
             $query->with('itemable:id,code')->where('itemable_type', 'App\Models\Tsr');
         }, 'or:id,op_id,number','or.detail','or.wallet'])
+         ->whereHasMorph('items.itemable', [\App\Models\Tsr::class], function ($q) {
+        $q->whereMonth('created_at', $this->month)
+          ->whereYear('created_at', $this->year);
+    })
+
         ->where('payorable_type', 'App\Models\Customer')
         ->where('status_id',7)
         ->where('agency_id',$this->laboratory)
-        ->whereMonth('created_at',$this->month)
-        ->whereYear('created_at',$this->year)
+        // ->whereMonth('created_at',$this->month)
+        // ->whereYear('created_at',$this->year)
         ->get();
 
         $ops = [];
