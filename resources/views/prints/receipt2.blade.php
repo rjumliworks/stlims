544aae354eb0a0adadebc6a884449b6ea017b3dd
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-12-05 17:09:05 +0800

    Update receipt2.blade.php

--- resources/views/prints/receipt2.blade.php
+++ resources/views/prints/receipt2.blade.php
@@ -7,7 +7,7 @@
             html * {
                 font-family: Times New Roman;
                 /* font-weight: bold; */
-                margin-top: 63px;
+                margin-top: 67px;
                 margin-left: -7px;
             }
             table,
