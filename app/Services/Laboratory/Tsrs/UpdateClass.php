b9bece7cd1dfdda20312833aaf55a15b9fc7abc8
kradworkz <80160702+kradworkz@users.noreply.github.com>
2025-12-11 16:44:02 +0800

    aw

--- app/Services/Laboratory/Tsrs/UpdateClass.php
+++ app/Services/Laboratory/Tsrs/UpdateClass.php
@@ -83,12 +83,12 @@ class UpdateClass
 
     public function update($request){
         $data = Tsr::with('payment')->where('id',$request->id)->first();
-         
+        $originalTime = Carbon::parse($data->created_at)->format('H:i:s');
         if($data->status_id == 1 || $data->status_id == 2){
             $data->customer_id = $request->customer['value'];
             $data->conforme_id = $request->conforme['value'];
             $data->purpose_id = $request->purpose_id;
-            $data->created_at = Carbon::parse($request->created_at)->setTime(8, 0);
+            $data->created_at = Carbon::parse($request->created_at . ' ' . $originalTime);
             $data->due_at = $request->due_at;
             $data->laboratory_id = $request->laboratory_id;
             $data->save();
