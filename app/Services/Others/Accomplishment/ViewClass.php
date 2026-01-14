b5fbb2eae138b75b25a9999d26ac20de570399d8
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-01-14 09:22:44 +0800

    Update ViewClass.php

--- app/Services/Others/Accomplishment/ViewClass.php
+++ app/Services/Others/Accomplishment/ViewClass.php
@@ -22,7 +22,7 @@ class ViewClass
     }
 
     public function show($request,$agencies){
-        $year = date('Y');
+        $year = 2025;
         $data = Target::with('breakdowns.laboratory')->where('year',$year)->where('agency_id',$this->agency)->first();
         if($data){
            return $this->view($year,$agencies);
