9297cda0e9576274c965f71d30a2ba791cf429c2
kradworkz <80160702+kradworkz@users.noreply.github.com>
2026-02-12 10:50:02 +0800

    Update list-sample.blade.php

--- resources/views/qrcodes/list-sample.blade.php
+++ resources/views/qrcodes/list-sample.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <style>
+            
             html * {
                 font-family:Arial, Helvetica, sans-serif;
                 margin: 0;
@@ -78,6 +79,15 @@
                 left: -21; /* 7 cm from the left */
                 border-top: 1px solid black; /* Thickness and color of the line */
             }
+            .label-header {
+                position: relative;
+                margin-left: 0px; /* change this when needed */
+            }
+                        .label-header.shifted {
+                margin-left: 20px; 
+                margin-top: -10px;
+                margin-bottom: 15px; /* adjust as needed */
+            }
         </style>
     </head>
 
@@ -87,36 +97,70 @@
             $count = $count - 1;
         @endphp
         @foreach($lists as $index2=>$list)
-        <img class="image" src="<?php echo $list['qrCodeImage']; ?>" alt="QR Code"/>
-        <div class="code">{{$list['sample_code']}}  @if (strlen($list['sample_name']) <= 15)<span style="font-size: 10px;">({{$list['sample_name']}})</span>@endif</div>
-        <div class="received">Received: {{$list['created_at']}}</div>
-        <div class="due">Due at: {{$list['due_at']}}</div>
-        @if (strlen($list['sample_name']) > 15)
-            <div class="desc"><span style="font-size: 10px;">{{$list['sample_name']}}</span></div>
-            <div class="breakline2"></div>
-            <div class="names2">
-                <ul>
-                    @foreach($list['testnames'] as $index=>$name)
-                    <li>{{$name}}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @if($index2 < ($count))
-            <div class="page-break"></div>
+            <img class="image" src="<?php echo $list['qrCodeImage']; ?>" alt="QR Code"/>
+            <div class="code">{{$list['sample_code']}}  @if (strlen($list['sample_name']) <= 15)<span style="font-size: 10px;">({{$list['sample_name']}})</span>@endif</div>
+            <div class="received">Received: {{$list['created_at']}}</div>
+            <div class="due">Due at: {{$list['due_at']}}</div>
+            @if (strlen($list['sample_name']) > 15)
+                <div class="desc"><span style="font-size: 10px;">{{$list['sample_name']}}</span></div>
+                <div class="breakline2"></div>
+                <div class="names2">
+                    <ul>
+                        @php
+                            $chunks = array_chunk($list['testnames'], 12);
+                        @endphp
+
+                        @foreach($chunks as $chunkIndex => $chunk)
+                                @foreach($chunk as $name)
+                                    <li>{{$name}}</li>
+                                @endforeach
+
+                            @if($chunkIndex < count($chunks) - 1)
+                                <div class="page-break"></div>
+                                
+                            @endif
+                        @endforeach
+                    </ul>
+                </div>
+                @if($index2 < ($count))
+                <div class="page-break"></div>
+                @endif
+            @else
+                <div class="breakline"></div>
+                <div class="names">
+                    <ul>
+                        @php
+                            $chunks = array_chunk($list['testnames'], 13);
+                        @endphp
+
+                        @foreach($chunks as $chunkIndex => $chunk)
+                                @foreach($chunk as $name)
+                                    <li>{{$name}}</li>
+                                @endforeach
+                            @if($chunkIndex < count($chunks) - 1)
+                                <div class="page-break"></div>
+                              
+                                <div class="label-header shifted">
+                                    <img class="image" src="<?php echo $list['qrCodeImage']; ?>" alt="QR Code"/>
+                                    <div class="code">
+                                        {{$list['sample_code']}}
+                                        @if (strlen($list['sample_name']) <= 15)
+                                            <span style="font-size: 10px;">({{$list['sample_name']}})</span>
+                                        @endif
+                                    </div>
+                                    <div class="received">Received: {{$list['created_at']}}</div>
+                                    <div class="due">Due at: {{$list['due_at']}}</div>
+                                    <div class="breakline"></div>
+                                </div>
+                              
+                            @endif
+                        @endforeach
+                    </ul>
+                </div>
+                @if($index2 < ($count))
+                <div class="page-break"></div>
+                @endif
             @endif
-        @else
-            <div class="breakline"></div>
-            <div class="names">
-                <ul>
-                    @foreach($list['testnames'] as $index=>$name)
-                    <li>{{$name}}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @if($index2 < ($count))
-            <div class="page-break"></div>
-            @endif
-        @endif
         @endforeach
     </body>
 </html>
